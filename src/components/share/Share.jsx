37b8d7f5b628ef3a4a6bf0b92795c7a43b4aac19
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2025-03-22 23:11:05 -0400

    update so far with a share

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,50 @@
+import "./share.css";
+import {
+	MdPermMedia,
+	MdLabel,
+	MdAddLocation,
+	MdEmojiEmotions,
+} from "react-icons/md";
+
+export default function Share() {
+	return (
+		<div className="share">
+			<div className="shareWrapper">
+				<div className="shareTop">
+					<img
+						className="shareProfileImg"
+						src="/assets/person/1.jpeg"
+						alt=""
+					/>
+					<input
+						placeholder="What's on your mind?"
+						type="text"
+						className="shareInput"
+					/>
+				</div>
+				<hr className="shareHr" />
+				<div className="shareBottom">
+					<div className="shareOptions">
+						<div className="shareOption">
+							<MdPermMedia className="shareIcon" />
+							<span className="shareOptionText">Photo or Video</span>
+						</div>
+						<div className="shareOption">
+							<MdLabel className="shareIcon" />
+							<span className="shareOptionText">Tag</span>
+						</div>
+						<div className="shareOption">
+							<MdAddLocation className="shareIcon" />
+							<span className="shareOptionText">Location</span>
+						</div>
+						<div className="shareOption">
+							<MdEmojiEmotions className="shareIcon" />
+							<span className="shareOptionText">Feelings</span>
+						</div>
+					</div>
+					<button className="shareButton">Share</button>
+				</div>
+			</div>
+		</div>
+	);
+}
