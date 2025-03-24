2252b3aec38bdd51dc4c8d57afc5367399847790
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2025-03-24 14:59:32 -0400

    finshed with posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -22,11 +22,29 @@ export default function Post() {
 				<div className="postCenter">
 					<span className="postText">Hey! It's my first post</span>
 					<img
-						src="/assets/post/1.jpeg"
+						className="postImg"
+						src="assets/post/1.jpeg"
 						alt=""
 					/>
 				</div>
-				<div className="postBottom"></div>
+				<div className="postBottom">
+					<div className="postBottomLeft">
+						<img
+							className="likeIcon"
+							src="assets/like.png"
+							alt=""
+						/>
+						<img
+							className="likeIcon"
+							src="assets/heart.png"
+							alt=""
+						/>
+						<span className="postLikeCounter">32 people liked it</span>
+					</div>
+					<div className="postBottomRight">
+						<span className="postCommentText">9 comments</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
