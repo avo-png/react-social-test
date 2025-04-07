9ab7458c5466b6bb3c34f4315a154734d016dbaa
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2025-04-07 00:43:15 -0400

    add login and resiter page

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,38 +1,38 @@
 import "./rightbar.css";
-import { Users } from "../../dummyData";
-import Online from "../online/Online";
+// import { Users } from "../../dummyData";
+// import Online from "../online/Online";
 
 export default function Rightbar({ profile }) {
-	const HomeRightbar = () => {
-		return (
-			<>
-				<div className="birthdayContainer">
-					<img
-						className="birthdayImg"
-						src="assets/gift.png"
-						alt=""
-					/>
-					<span className="birthdayText">
-						<b>Pola Foster</b> and <b>3 other friends</b> have birthday today
-					</span>
-				</div>
-				<img
-					src="assets/ad.png"
-					alt=""
-					className="rightbarAd"
-				/>
-				<h4 className="rightbarTitle">Online Friends</h4>
-				<ul className="rightbarFriendsList">
-					{Users.map((u) => (
-						<Online
-							key={u.id}
-							user={u}
-						/>
-					))}
-				</ul>
-			</>
-		);
-	};
+	// const HomeRightbar = () => {
+	// 	return (
+	// 		<>
+	// 			<div className="birthdayContainer">
+	// 				<img
+	// 					className="birthdayImg"
+	// 					src="assets/gift.png"
+	// 					alt=""
+	// 				/>
+	// 				<span className="birthdayText">
+	// 					<b>Pola Foster</b> and <b>3 other friends</b> have birthday today
+	// 				</span>
+	// 			</div>
+	// 			<img
+	// 				src="assets/ad.png"
+	// 				alt=""
+	// 				className="rightbarAd"
+	// 			/>
+	// 			<h4 className="rightbarTitle">Online Friends</h4>
+	// 			<ul className="rightbarFriendsList">
+	// 				{Users.map((u) => (
+	// 					<Online
+	// 						key={u.id}
+	// 						user={u}
+	// 					/>
+	// 				))}
+	// 			</ul>
+	// 		</>
+	// 	);
+	// };
 
 	const ProfileRightbar = () => {
 		return (
